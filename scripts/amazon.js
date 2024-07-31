7edda1979d4f25d6443e06d901361fcfcd60b079
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-07-31 18:01:51 -0500

    updating amazon project

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,7 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
 
-let productsHTML = '';
+
+let productsHTML = '';  // This is the variable that will hold the HTML for all the products
+
 products.forEach((products) => {
     productsHTML = productsHTML +=` <div class="product-container">
           <div class="product-image-container">
@@ -70,7 +47,7 @@ products.forEach((products) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart">
             Add to Cart
           </button>
         </div>`
@@ -79,3 +56,9 @@ products.forEach((products) => {
 console.log(productsHTML)
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach(button => {
+   button.addEventListener('click', () => {
+     console.log('Add to cart clicked')
+   })
+})
