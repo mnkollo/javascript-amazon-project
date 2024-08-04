461f007c5b34a23d73ec9482321f6ba36ea22e59
mnkolloQAEngineer <mnkollo@gmail.com>
2024-08-03 22:31:59 -0500

    added products module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = ""; // This is the variable that will hold the HTML for all the products
 
