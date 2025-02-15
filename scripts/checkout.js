ac52a39d36091595014251732fe43db45a561a2f
mnkolloQAEngineer <mnkollo@gmail.com>
2025-02-15 13:42:17 -0600

    working on 18.15

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,7 @@ import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 hello();
 
 const today = dayjs();
