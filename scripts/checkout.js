66b36affdce615ef945b5621ec356462d888fa8a
mnkollo <73963114+mnkollo@users.noreply.github.com>
2025-02-16 13:12:39 -0600

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,139 +1,75 @@
-import { cart, removeFromCart } from "../data/cart.js";
-import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
-import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
-import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../data/deliveryOptions.js";
-//import '../data/backend-practice.js';
-hello();
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
+// import '../data/cart-class.js';
+// import '../data/backend-practice.js';
 
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-console.log(deliveryDate.format("dddd, MMMM D"));
+async function loadPage() {
+  try {
+    // throw 'error1';
 
-let cartSummaryHTML = "";
+    await loadProductsFetch();
 
-cart.forEach((cartItem) => {
-  const productId = cartItem.productId;
+    const value = await new Promise((resolve, reject) => {
+      // throw 'error2';
+      loadCart(() => {
+        // reject('error3');
+        resolve('value3');
+      });
+    });
 
-  let matchingProduct;
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
 
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
-
-  cartSummaryHTML += `
-    <div class="cart-item-container
-      js-cart-item-container-${matchingProduct.id}">
-      <div class="delivery-date">
-        Delivery date: Tuesday, June 21
-      </div>
-
-      <div class="cart-item-details-grid">
-        <img class="product-image"
-          src="${matchingProduct.image}">
-
-        <div class="cart-item-details">
-          <div class="product-name">
-            ${matchingProduct.name}
-          </div>
-          <div class="product-price">
-            $${formatCurrency(matchingProduct.priceCents)}
-          </div>
-          <div class="product-quantity">
-            <span>
-              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-            </span>
-            <span class="update-quantity-link link-primary">
-              Update
-            </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
-              matchingProduct.id
-            }">
-              Delete
-            </span>
-          </div>
-        </div>
-
-        <div class="delivery-options">
-          <div class="delivery-options-title">
-            Choose a delivery option:
-          </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          ${deliveryOptionsHTML(matchingProduct)}
-        </div>
-      </div>
-    </div>
-  `;
-});
-
-function deliveryOptionsHTML(matchingProduct, cartItem) {
-  let html = '';
-
-  deliveryOptions.forEach(() => {
-    const today = dayjs(); 
-    const deliveryDate = today.add(deliveryOptions.deliveryDays, "days");  
-    const dateString = deliveryDate.format("dddd, MMMM D");  
-    const priceString = deliveryOptions.priceCents === 0 ? "FREE" : `$${formatCurrency(deliveryOptions.priceCents)} -`;
-    const isChecked = deliveryOptions.id === cartItem.deliveryOptionsId;
-    html +=
-    `
-      <div class="delivery-option">
-              <input type="radio"
-                ${isChecked ? "checked" : ""}
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  ${dateString}
-                </div>
-                <div class="delivery-option-price">
-                  ${priceString} Shipping
-                </div>
-              </div>
-            </div>
-            `;
-  });
-  return html;
+  renderOrderSummary();
+  renderPaymentSummary();
 }
+loadPage();
 
-document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+/*
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
 
-document.querySelectorAll(".js-delete-link").forEach((link) => {
-  link.addEventListener("click", () => {
-    const productId = link.dataset.productId;
-    removeFromCart(productId);
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
 
-    const container = document.querySelector(
-      `.js-cart-item-container-${productId}`
-    );
-    container.remove();
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
   });
 });
+*/
